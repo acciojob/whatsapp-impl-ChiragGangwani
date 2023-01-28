9d1346543d537b55ae48ee46f1bfeb3f70d3621b
ChiragGangwani <chirag.sgr2001@gmail.com>
2023-01-28 17:42:32 +0530

    First

--- src/main/java/com/driver/WhatsappRepository.java
+++ src/main/java/com/driver/WhatsappRepository.java
@@ -26,4 +26,101 @@ public class WhatsappRepository {
         this.customGroupCount = 0;
         this.messageId = 0;
     }
+
+    public String createUser(String name, String mobile) {
+        if(userMobile.contains(mobile))
+            return null;
+        userMobile.add(mobile);
+        return "SUCCESS";
+    }
+
+    public Group createGroup(List<User> users) {
+        if(users.size()==2){
+            Group g=new Group(users.get(1).getName(),2);
+            adminMap.put(g,users.get(0));
+            groupUserMap.put(g,users);
+            return g;
+        }
+        customGroupCount++;
+        Group g =new Group("Group "+customGroupCount,users.size());
+        adminMap.put(g,users.get(0));
+        groupUserMap.put(g,users);
+        return g;
+    }
+
+    public int createMessage(String content) {
+        messageId++;
+        Message m=new Message(messageId,content);
+        return messageId;
+    }
+
+    public int sendMessage(Message message, User sender, Group group) {
+        if(!groupUserMap.containsKey(group))
+            return -1;
+        List <User> temp=groupUserMap.get(group);
+        for(User u:temp){
+            if(u==sender) {
+                List<Message> ans = groupMessageMap.get(group);
+                ans.add(message);
+                groupMessageMap.put(group, ans);
+                senderMap.put(message, sender);
+                return groupMessageMap.get(group).size();
+            }
+        }
+        return -2;
+    }
+
+    public String changeAdmin(User approver, User user, Group group) {
+        if(!groupUserMap.containsKey(group))
+            return "group";
+        if(adminMap.get(group)!=approver)
+            return "approver";
+        List<User>temp=groupUserMap.get(group);
+        for(User x:temp){
+            if(x==user){
+                adminMap.put(group,user);
+                return "SUCCESS";
+            }
+        }
+        return "user";
+    }
+
+    public int removeUser(User user) {
+        for(Group g:groupUserMap.keySet()){
+            List<User>temp=groupUserMap.get(g);
+            for(User u:temp){
+                if(u==user && adminMap.get(g)==user){
+                    return -2;
+                }
+                else if(u==user){
+                    temp.remove(user);
+                    List<Message> an = groupMessageMap.get(g);
+                    for(Message m:senderMap.keySet()){
+                        if(senderMap.get(m)==user) {
+                            senderMap.remove(m);
+                            for (Message e : an) {
+                                if (e == m)
+                                    an.remove(e);
+                            }
+                            groupMessageMap.put(g, an);
+                        }
+                    }
+                    groupUserMap.put(g,temp);
+                    return temp.size()+an.size()+senderMap.size();
+                }
+            }
+        }
+        return -1;
+    }
+
+    public String findMessage(Date start, Date end, int k) {
+        List<Message>temp=new ArrayList<>();
+        for(Message m: senderMap.keySet()){
+            if(m.getTimestamp().after(start)&& m.getTimestamp().before(end))
+            temp.add(m);
+        }
+        if(temp.size()<k)
+            return "null";
+            return temp.get(k).getContent();
+    }
 }
